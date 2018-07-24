68663ec56fbaeb98a032a701cdcdd9bc455f2b75
Tyler <TEBcoyo1@gmail.com>
2018-07-24 11:44:06 -0500

    Minor preprocessor fix

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -21,7 +21,7 @@ limitations under the License.
 #include <iostream>
 
 void bitboards::initStandardBoard() {
-#ifdef generateBoards
+//#ifdef generateBoards
   const char board[64] = {
     'r','n','b','q','k','b','n','r',
     'p','p','p','p','p','p','p','p',
