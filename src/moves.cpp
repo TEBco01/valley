e224c5c7aa89d05c3b5a74eda5466cc1f42c7719
Tyler <TEBcoyo1@gmail.com>
2019-03-22 21:07:59 -0500

    Fixed an issue with en passant.

--- src/moves.cpp
+++ src/moves.cpp
@@ -165,7 +165,7 @@ linkedMoveList possibleMovesWPawns(move lastMove, const bitboards game, const ex
   // En passant
 	if(!(lastMove.start == 0 && lastMove.end == 0)) { // Was there a last move?
 		if(24 <= lastMove.end && lastMove.end <= 31) { // Did it end in our range?
-			if(pieceAtSquare(game.BP, lastMove.end)) { // Was it an enemy pawn?
+			if(pieceAtSquare(game.BP, lastMove.end) && 8 <= lastMove.start && lastMove.start <= 15) { // Was it an enemy pawn? Did this pawn move two?
 				if(lastMove.end >= 25) // Was it in bounds on the left?
 					if(pieceAtSquare(game.WP, lastMove.end - 1)) // Is there a friendly piece on the left side?
 						possibleMoves.create(lastMove.end - 1, lastMove.end - 8);
@@ -215,7 +215,7 @@ linkedMoveList possibleMovesBPawns(move lastMove, const bitboards game, const ex
   // En passant
 	if(!(lastMove.start == 0 && lastMove.end == 0)) { // Was there a last move?
 		if(32 <= lastMove.end && lastMove.end <= 39) { // Did it end in our range?
-			if(pieceAtSquare(game.WP, lastMove.end)) { // Was it an enemy pawn?
+			if(pieceAtSquare(game.WP, lastMove.end)  && 48 <= lastMove.start && lastMove.start <= 55) { // Was it an enemy pawn? Did this pawn move two?
 				if(lastMove.end >= 33) // Was it in bounds on the left?
 					if(pieceAtSquare(game.BP, lastMove.end - 1)) // Is there a friendly piece on the left side?
 						possibleMoves.create(lastMove.end - 1, lastMove.end + 8, 10);
