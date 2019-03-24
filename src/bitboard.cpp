84464f7f5093a741e1d0a1aeb9f2711e48fea2a3
Tyler <TEBcoyo1@gmail.com>
2019-03-24 16:48:49 -0500

    Additional code cleanup.

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -21,7 +21,6 @@ limitations under the License.
 #include <iostream>
 
 void bitboards::initStandardBoard() {
-//#ifdef generateBoards
   const char board[64] = {
     'r','n','b','q','k','b','n','r',
     'p','p','p','p','p','p','p','p',
@@ -45,56 +44,8 @@ void bitboards::initStandardBoard() {
   BR = arrayToBitboard(board, 'r');
   BQ = arrayToBitboard(board, 'q');
   BK = arrayToBitboard(board, 'k');
-
-  /*const char board2[64] = {
-    '%','U','b','q','k','b','A','A',
-    'U','U','p','p','p','p','A','A',
-    'U','U',' ',' ',' ',' ','A','A',
-    'Y','Y','Y','Y','Y','Y','Y','Y',
-    'X','X','X','X','X','X','X','X',
-    'U','U',' ',' ',' ',' ','A','A',
-    'U','U','P','P','P','P','A','A',
-    'Z','Z','Z','Z','Z','Z','A','A'
-  };
-
-  std::cout << BR << std::endl;
-  std::cout << "Trouble: " << arrayToBitboard(board2, '%') << std::endl;
-  */
-
-  /*
-  std::cout << WP << std::endl;
-  std::cout << WN << std::endl;
-  std::cout << WB << std::endl;
-  std::cout << WR << std::endl;
-  std::cout << WQ << std::endl;
-  std::cout << WK << std::endl;
-  std::cout << BP << std::endl;
-  std::cout << BN << std::endl;
-  std::cout << BB << std::endl;
-  std::cout << BR << std::endl;
-  std::cout << BQ << std::endl;
-  std::cout << BK << std::endl;
-  */
-
-/*
-  // Precomputed values
-  // These are incorrect, actually
-  WP = 130560ULL;
-  WN = 130692ULL;
-  WB = 130764ULL;
-  WR = 131022ULL;
-  WQ = 131054ULL;
-  WK = 131070ULL;
-  BP = 33554430ULL;
-  BN = 18446744071662731262ULL;
-  BB = 18446744072870690814ULL;
-  BR = 18446744072904245247ULL;
-  BQ = 18446744073441116159ULL;
-  BK = 18446744073709551615ULL;
-*/
 }
 
-//#ifdef generateBoards
 U64 bitboards::arrayToBitboard(const char array[64], char character) {
   U64 value = 0;
   for (int i=0; i < 64; i++) {
@@ -111,22 +62,7 @@ void bitboards::bitboardToArray(const U64 bitboard, char array[64], char charact
       array[i] = character;
     }
   }
-
-  // Alternate method devised when debugging
-  /*U64 bitboardCopy = bitboard;
-
-  int i = 63;
-  U64 tempCopy = 0;
-  while(bitboard != 0 && i >= 0) {
-    tempCopy = bitboardCopy;
-    bitboardCopy >>= 1;
-    if(tempCopy != (bitboardCopy << 1) ) {
-      array[i] = character;
-    }
-    i--;
-  }*/
 }
-//#endif
 
 void bitboards::convertToStandardArray(char array[64]) {
   for (int i=0; i < 64; i++) {
